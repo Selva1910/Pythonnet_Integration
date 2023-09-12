0a42873383045e3d01e63a6a190fb213edd91f08
Selvaraj Balakrishnan <bselva1910@gmail.com>
2023-09-12 23:26:31 +0530

    Things Done: 1. Installed PythonNet using NugetPackage Manager 2. Added the Simple Python Code File to Test. 3. Added a Simple Function to Initialize and Call Python Function using InvokeMethod().

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Python.Runtime;
+
+RunScript("pyScript");
+
+static void RunScript(string scriptName)
+{
+    Runtime.PythonDLL = @"C:\Users\bselv\AppData\Local\Programs\Python\Python311\python311.dll";
+
+    Console.WriteLine("Python Engine Initializing!...\r\n");
+    PythonEngine.Initialize();
+
+    using (Py.GIL())
+    {
+        var pythonScript = Py.Import(scriptName);
+        Console.WriteLine("Python Code Executing!...");
+        var logResult = pythonScript.InvokeMethod("LogStatement");
+        Console.WriteLine(logResult);
+        int num1 = 2;
+        int num2 = 3;
+        var AddResult = pythonScript.InvokeMethod("AddTwo", num1.ToPython(), num2.ToPython());
+        Console.WriteLine($"\r\n Passed Values Number 1: <color>{num1} \n Number 2 : {num2} \n Added Value : {AddResult}");
+    }
+}
---
+++ pyScript.py
@@ -0,0 +1,5 @@
+def LogStatement():
+	return "This is a Sample LogStatement!"
+
+def AddTwo(a,b):
+	return a+b
